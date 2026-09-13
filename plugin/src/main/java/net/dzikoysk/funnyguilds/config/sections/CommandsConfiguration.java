b8b25e497029b98b2ed63e227e7e602744cae019
zrdzn <zrdzn9@gmail.com>
2026-09-13 13:10:27 +0200

    Add upgrade command to configuration

--- plugin/src/main/java/net/dzikoysk/funnyguilds/config/sections/CommandsConfiguration.java
+++ plugin/src/main/java/net/dzikoysk/funnyguilds/config/sections/CommandsConfiguration.java
@@ -64,6 +64,8 @@ public class CommandsConfiguration extends OkaeriConfig {
     public FunnyCommand setbase = new FunnyCommand("ustawbaze", Collections.singletonList("ustawdom"));
     @Comment("")
     public FunnyCommand pvp = new FunnyCommand("pvp", Collections.singletonList("ustawpvp"));
+    @Comment("")
+    public FunnyCommand upgrades = new FunnyCommand("ulepszenia", Collections.singletonList("upgrades"));
 
     @Comment
     @Comment("Komendy administratora")
